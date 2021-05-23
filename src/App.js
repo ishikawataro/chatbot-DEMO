7510fdd32ebcc73a125f27f297c536996cc42659
ishikawataro <84612285+ishikawataro@users.noreply.github.com>
2021-05-23 10:11:56 +0900

    OK

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GitHub
+          Learn github
         </a>
       </header>
     </div>
