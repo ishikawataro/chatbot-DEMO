2980f2db0fdb1939b7aca6e646c77cd9714a27fd
ishikawataro <84612285+ishikawataro@users.noreply.github.com>
2021-05-23 10:22:24 +0900

    GIIT

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn github
+          Learn GIIIIT!!
         </a>
       </header>
     </div>
